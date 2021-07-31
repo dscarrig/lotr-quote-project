261298f427d0964a1835d11a4dacb1f9233cbcff
Scott <dcarrig1@gmail.com>
2021-07-30 22:06:13 -0500

    Test for just base

--- app.py
+++ app.py
@@ -130,10 +130,13 @@ def home():
     - anon users: no messages
     - logged in: 100 most recent messages of followed_users
     """
-    if g.user:
-        return render_template('home.html', user = g.user)
-    else:
-        return render_template('home-anon.html')
+    
+    #if g.user:
+    #    return render_template('home.html', user = g.user)
+    #else:
+    #    return render_template('home-anon.html')
+
+    return render_template('base.html')
 
 
 ##############################################################################
